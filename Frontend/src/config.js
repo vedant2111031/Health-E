8d82eae51bbd2fb8346b16834b767324ec6d027b
Vedant pandey <pvedant69@gmail.com>
2024-12-15 14:28:56 +0530

    Config added

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
-// export const BASE_URL="https://health-e-oe6b.onrender.com/api/v1"
-export const BASE_URL="http://localhost:8000/api/v1"
+export const BASE_URL="https://health-e-oe6b.onrender.com/api/v1"
+// export const BASE_URL="http://localhost:8000/api/v1"
 
 export const getToken = () => localStorage.getItem('token');   
