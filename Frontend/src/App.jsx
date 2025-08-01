ab35b59a7a3eff9e40abf642408e6ca0930ebd1a
Vedant pandey <113361700+vedant2111031@users.noreply.github.com>
2025-08-01 13:12:44 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,11 @@
-// App.jsx
+// src/App.jsx
+
 import './App.css';
 import Layout from './layout/Layout';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { pushPageLoadData } from './utils/datalayer';
-
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user')) || null;
-    const pageName = location.pathname === '/' ? 'Home' : location.pathname.replace('/', '');
-    pushPageLoadData({ pageName, user });
-  }, [location]);
-
   return <Layout />;
 }
 
