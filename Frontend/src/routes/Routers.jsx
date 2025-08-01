4ab329c4eeba813bf0eeb19f46385afddb5931e1
Vedant pandey <113361700+vedant2111031@users.noreply.github.com>
2025-08-01 14:50:17 +0530

    Update Routers.jsx

--- Frontend/src/routes/Routers.jsx
+++ Frontend/src/routes/Routers.jsx
@@ -16,8 +16,14 @@ import TumorDetection from "../pages/TumorDetection";
 import AlzheimerDetection from "../pages/AlzheimerDetection";
 import HemorrhageDetection from "../pages/HemorrhageDetection";
 import Result from "../pages/MRIResult";
+import { pushPageViewData } from "../components/pageViewTracker"; 
 
 function Routers() {
+   const location = useLocation();
+
+  useEffect(() => {
+    pushPageViewData(location.pathname);
+  }, [location]);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
