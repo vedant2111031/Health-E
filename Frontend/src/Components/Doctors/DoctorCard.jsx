7b8840cb6450fc0cc0677b2ae4a25e4383e2e200
Vedant pandey <113361700+vedant2111031@users.noreply.github.com>
2025-08-06 13:44:12 +0530

    Update DoctorCard.jsx

--- Frontend/src/Components/Doctors/DoctorCard.jsx
+++ Frontend/src/Components/Doctors/DoctorCard.jsx
@@ -45,12 +45,30 @@ const DoctorCard = ({ doctor }) => {
             At {experiences && experiences[0]?.hospital} 
           </p>
         </div>
-        <Link
-          to={`/doctors/${doctor._id}`}
-          className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none"
-        >
-          <BsArrowRight className="group-hover:text-white w-6 h-5" />
-        </Link>
+       <Link
+  to={`/doctors/${doctor._id}`}
+  onClick={() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "web.webinteractionlink.click",
+      web: {
+        componentname: "doctorcard",
+        interactiontype: "link click",
+        link: {
+          section: "doctor list",
+          text: name.toLowerCase(),             // doctor name in lowercase
+          specialization: specialization.toLowerCase(),
+          hospital: experiences && experiences[0]?.hospital?.toLowerCase(),
+          url: `/doctors/${doctor._id}`
+        }
+      }
+    });
+  }}
+  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+>
+  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+</Link>
+
       </div>
     </div>
   );
