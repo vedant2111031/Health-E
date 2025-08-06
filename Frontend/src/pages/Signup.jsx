d8b5aea2d1f7be4f9e12d58176e9f7a7118f5541
Vedant pandey <113361700+vedant2111031@users.noreply.github.com>
2025-08-06 15:14:54 +0530

    Update Signup.jsx

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -155,4 +155,107 @@ const Signup = () => {
                   name="name"
                   value={formData.name}
                   onChange={handleInputChange}
-                  className="w-full
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[17px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[17px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[17px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5 flex items-center justify-between">
+                <label className="text-headingColor font-bold text-[16px] leading-7">
+                  Are you a:
+                  <select
+                    name="role"
+                    value={formData.role}
+                    onChange={handleInputChange}
+                    className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="patient">Patient</option>
+                    <option value="doctor">Doctor</option>
+                  </select>
+                </label>
+
+                <label className="text-headingColor font-bold text-[16px] leading-7">
+                  Gender:
+                  <select
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleInputChange}
+                    className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="">Select</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Others</option>
+                  </select>
+                </label>
+              </div>
+              <div className="mb-5 flex items-center gap-3">
+                {selectedFile && (
+                  <figure className="w-[40px] h-[40px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                    <img src={previewURL} alt="" className="w-full rounded-full" />
+                  </figure>
+                )}
+
+                <div className="relative w-[160px] h-[40px]">
+                  <input
+                    type="file"
+                    name="photo"
+                    id="customFile"
+                    onChange={handelFileInputChange}
+                    accept=".jpg, .png"
+                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                  />
+                  <label
+                    htmlFor="customFile"
+                    className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+                  >
+                    Upload Photo
+                  </label>
+                </div>
+              </div>
+              <div className="mt-7">
+                <button
+                  disabled={loading}
+                  type="submit"
+                  className="w-full bg-primaryColor text-white text-[16px] leading[30px] rounded-lg px-4 py-3"
+                >
+                  {loading ? <HashLoader size={35} color="#ffffff" /> : "Sign up"}
+                </button>
+              </div>
+
+              <p className="mt-5 text-textColor text-center">
+                Have an account?
+                <Link to="/login" className="text-primaryColor font-medium ml-1">
+                  Login
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
