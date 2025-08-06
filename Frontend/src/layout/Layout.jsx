20744dc94147bbf9a71138fb4d5afea46d2efcf6
Vedant pandey <113361700+vedant2111031@users.noreply.github.com>
2025-08-06 13:23:07 +0530

    Update Layout.jsx

--- Frontend/src/layout/Layout.jsx
+++ Frontend/src/layout/Layout.jsx
@@ -3,12 +3,14 @@ import Header from '../Components/Header/Header'
 import Footer from '../Components/Footer/Footer'
 import Routers from '../routes/Routers'
 import Chatbot from '../Components/Chatbot/Chatbot'
+import PageViewTracker from '/PageViewTracker'
 
 function Layout() {
   return (
     <>
     <Header/>
     <main>
+      <PageViewTracker />
       <Routers/>
       <Chatbot/>
     </main>
