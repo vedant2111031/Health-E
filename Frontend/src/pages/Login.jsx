cd9eeffafec64dc40b157cce7be3299da2220f1d
Vedant pandey <113361700+vedant2111031@users.noreply.github.com>
2025-08-14 21:41:04 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
             form: {
               name: "login form",
               formType: "authentication",
-              status: "login success"
+              status: "login success",
             },
           },
         },
