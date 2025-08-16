276fcedc8bf0a9a58e147e1e299e885f72ae8a9b
Vedant pandey <113361700+vedant2111031@users.noreply.github.com>
2025-08-16 18:48:30 +0530

    Update Dashbord.jsx

--- Frontend/src/Dashbord/doctor-account/Dashbord.jsx
+++ Frontend/src/Dashbord/doctor-account/Dashbord.jsx
@@ -1,114 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import Loader from "../../Components/Loader/Loading";
-// import Error from "../../Components/Error/Error";
-// import useFetchData from "../../hooks/useFetchData";
-// import { BASE_URL } from "../../config";
-// import Tabs from "./Tabs";
-// import starIcon from "../../assets/images/Star.png";
-// import DoctorAbout from "../../pages/Doctors/DoctorAbout";
-// import Profile from "./Profile";
-// import Appointments from "./Appointments";
-
-// const Dashbord = () => {
-//   const { data, loading, error, fetchData } = useFetchData(
-//     `${BASE_URL}/doctors/profile/me`
-//   );
-
-//   const [tab, setTab] = useState("overview");
-
-//   useEffect(() => {
-//     if (!data) {
-//       fetchData();
-//     }
-//   }, [data, fetchData]);
-
-//   return (
-//     <section className="mt-10">
-//       <div className="max-w-[1170px] px-5 mx-auto">
-//         {loading && !error && <Loader />}
-//         {error && !loading && <Error />}
-//         {!loading && !error && data && (
-//           <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
-//             <Tabs tab={tab} setTab={setTab} />
-
-//             <div className="lg:col-span-2 ">
-//               {data.isApproved === "pending" && (
-//                 <div className="flex p-4 mb-4 text-yellow-800 bg-yellow-50 rounded-lg">
-//                   <svg
-//                     aria-hidden="true"
-//                     className="flex-shrink-0 w-5 h-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       fillRule="evenodd"
-//                       d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-//                       clipRule="evenodd"
-//                     ></path>
-//                   </svg>
-//                   <span className="sr-only">Info</span>
-//                   <div className="ml-3 text-sm font-medium">
-//                     To get approval please complete the profile with only
-//                     correct details. We&apos;ll review and approve within 3 days.
-//                   </div>
-//                 </div>
-//               )}
-
-//               <div className="mt-8">
-//                 {tab === "overview" && (
-//                   <div>
-//                     <div className="flex items-center gap-4 mb-10">
-//                       <figure className="max-w-[200px] max-h-[200px]">
-//                         <img
-//                           src={data?.photo}
-//                           alt=""
-//                           className="max-w-[200px] max-h-[200px]"
-//                         />
-//                       </figure>
-//                       <div>
-//                         <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
-//                           {data.specialization}
-//                         </span>
-//                         <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">
-//                           {data.name}
-//                         </h3>
-//                         <div className="flex items-center gap-[6px]">
-//                           <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
-//                             <img src={starIcon} alt="" />
-//                             ({data.averageRating})
-//                           </span>
-//                           <span className=" text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
-//                             {data.totalRating}
-//                           </span>
-//                         </div>
-//                         <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
-//                           {data?.bio}
-//                         </p>
-//                       </div>
-//                     </div>
-//                     <DoctorAbout
-//                       name={data.name}
-//                       about={data.about}
-//                       qualifications={data.qualifications}
-//                       experiences={data.experiences}
-//                     />
-//                   </div>
-//                 )}
-//                 {tab === "appointments" && (
-//                   <Appointments initialAppointments={data.appointments} />
-//                 )}
-//                 {tab === "settings" && <Profile doctorData={data} />}
-//               </div>
-//             </div>
-//           </div>
-//         )}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Dashbord;
 import React, { useState, useEffect } from "react";
 import Loader from "../../Components/Loader/Loading";
 import Error from "../../Components/Error/Error";
@@ -121,7 +10,8 @@ import Profile from "./Profile";
 import Appointments from "./Appointments";
 
 const Dashbord = () => {
-  const { data, loading, error, fetchData } = useFetchData(`${BASE_URL}/doctors/profile/me`
+  const { data, loading, error, fetchData } = useFetchData(
+    `${BASE_URL}/doctors/profile/me`
   );
 
   const [tab, setTab] = useState("overview");
@@ -139,14 +29,7 @@ const Dashbord = () => {
         {error && !loading && <Error />}
         {!loading && !error && data && (
           <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
-            
-            {/* Hamburger + Info Heading */}
-            <div className="flex items-center gap-3 lg:col-span-3 mb-6">
-              <Tabs tab={tab} setTab={setTab} />
-              <h2 className="text-lg font-semibold text-headingColor">
-                Information
-              </h2>
-            </div>
+            <Tabs tab={tab} setTab={setTab} />
 
             <div className="lg:col-span-2 ">
               {data.isApproved === "pending" && (
@@ -226,3 +109,120 @@ const Dashbord = () => {
 };
 
 export default Dashbord;
+// import React, { useState, useEffect } from "react";
+// import Loader from "../../Components/Loader/Loading";
+// import Error from "../../Components/Error/Error";
+// import useFetchData from "../../hooks/useFetchData";
+// import { BASE_URL } from "../../config";
+// import Tabs from "./Tabs";
+// import starIcon from "../../assets/images/Star.png";
+// import DoctorAbout from "../../pages/Doctors/DoctorAbout";
+// import Profile from "./Profile";
+// import Appointments from "./Appointments";
+
+// const Dashbord = () => {
+//   const { data, loading, error, fetchData } = useFetchData(`${BASE_URL}/doctors/profile/me`
+//   );
+
+//   const [tab, setTab] = useState("overview");
+
+//   useEffect(() => {
+//     if (!data) {
+//       fetchData();
+//     }
+//   }, [data, fetchData]);
+
+//   return (
+//     <section className="mt-10">
+//       <div className="max-w-[1170px] px-5 mx-auto">
+//         {loading && !error && <Loader />}
+//         {error && !loading && <Error />}
+//         {!loading && !error && data && (
+//           <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
+            
+//             {/* Hamburger + Info Heading */}
+//             <div className="flex items-center gap-3 lg:col-span-3 mb-6">
+//               <Tabs tab={tab} setTab={setTab} />
+//               <h2 className="text-lg font-semibold text-headingColor">
+//                 Information
+//               </h2>
+//             </div>
+
+//             <div className="lg:col-span-2 ">
+//               {data.isApproved === "pending" && (
+//                 <div className="flex p-4 mb-4 text-yellow-800 bg-yellow-50 rounded-lg">
+//                   <svg
+//                     aria-hidden="true"
+//                     className="flex-shrink-0 w-5 h-5"
+//                     fill="currentColor"
+//                     viewBox="0 0 20 20"
+//                     xmlns="http://www.w3.org/2000/svg"
+//                   >
+//                     <path
+//                       fillRule="evenodd"
+//                       d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+//                       clipRule="evenodd"
+//                     ></path>
+//                   </svg>
+//                   <span className="sr-only">Info</span>
+//                   <div className="ml-3 text-sm font-medium">
+//                     To get approval please complete the profile with only
+//                     correct details. We&apos;ll review and approve within 3 days.
+//                   </div>
+//                 </div>
+//               )}
+
+//               <div className="mt-8">
+//                 {tab === "overview" && (
+//                   <div>
+//                     <div className="flex items-center gap-4 mb-10">
+//                       <figure className="max-w-[200px] max-h-[200px]">
+//                         <img
+//                           src={data?.photo}
+//                           alt=""
+//                           className="max-w-[200px] max-h-[200px]"
+//                         />
+//                       </figure>
+//                       <div>
+//                         <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
+//                           {data.specialization}
+//                         </span>
+//                         <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">
+//                           {data.name}
+//                         </h3>
+//                         <div className="flex items-center gap-[6px]">
+//                           <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+//                             <img src={starIcon} alt="" />
+//                             ({data.averageRating})
+//                           </span>
+//                           <span className=" text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
+//                             {data.totalRating}
+//                           </span>
+//                         </div>
+//                         <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
+//                           {data?.bio}
+//                         </p>
+//                       </div>
+//                     </div>
+//                     <DoctorAbout
+//                       name={data.name}
+//                       about={data.about}
+//                       qualifications={data.qualifications}
+//                       experiences={data.experiences}
+//                     />
+//                   </div>
+//                 )}
+//                 {tab === "appointments" && (
+//                   <Appointments initialAppointments={data.appointments} />
+//                 )}
+//                 {tab === "settings" && <Profile doctorData={data} />}
+//               </div>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Dashbord;
